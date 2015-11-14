7da493065d8fcf4bd8d8182e5946216f350bf264
Patrik Hermansson <patrik@paheco.nu>
2015-11-14 15:41:15 +0100

    Update codesend_webgui.php

--- 3rdparty/tellstickd/codesend_webgui.php
+++ 3rdparty/tellstickd/codesend_webgui.php
@@ -10,6 +10,13 @@
   Name: <input type="text" name="code"><br>
   <input type="submit">
 </form>
-    
+    <form action="codesend_web.php" method="get">
+  Name: <input type="text" name="code" value="1048596"><br>
+  <input type="submit">
+</form>
+<form action="codesend_web.php" method="get">
+  Name: <input type="text" name="code" value="1048595"><br>
+  <input type="submit">
+</form>
   </body>
 </html>
