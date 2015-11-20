b073a18ce9abd17e7ee05f08b6a273781ef4ef8b
Patrik Hermansson <patrik@paheco.nu>
2015-11-20 14:18:56 +0100

    Mobile gui

--- 3rdparty/tellstickd/codesend_webgui.php
+++ 3rdparty/tellstickd/codesend_webgui.php
@@ -1,14 +1,42 @@
  <!doctype html>
 <html>
   <head>
-    <title></title>
+    <title>Codesend webgui</title>
     <meta content="">
+    <meta name=viewport content='width=700'>
+
 	<style rel="stylesheet" type="text/css">
 	.main {
   	margin: 15px;
   	padding: 7px 7px 7px 11px;
   	width: 120px;
 	}
+		@media screen and (max-width: 900px) {
+			html {
+	  			background-color: #82a43a; 
+			}
+			.main {
+			  	width: 300px;
+				height: 100px;
+				font-size: 24px;
+				color: D6D0CE;
+			}
+		}
+
+		#left {
+		float:left;
+		width:50%;
+		background:#2BF042;
+		}
+		#right {
+		float:right;
+		width:50%;
+		background:#E64915;
+		}
+		#status {
+		font-size:24px;
+		}
+
         </style>
 
   </head>
@@ -21,6 +49,9 @@
 	$("#btn1").click(function(){
 	    $.get('codesend_web.php', 'code=1052693', null, "xml")
 	    $('#status').text('Command sent'); 
+		 setTimeout(function(){
+		        $('#status').fadeOut(500);
+		    }, 3000);
 	});
   	$("#btn2").click(function(){
 	    $.get('codesend_web.php', 'code=1052692', null, "xml")
@@ -86,30 +117,27 @@
     </script>
 
 <div id="status"></div>
-<br>
-<button id="btn1" class="main">Vrum on</button>
-<button id="btn2" class="main">Vrum off</button>
-<br>
-<button id="btn3" class="main">Porch on</button>
-<button id="btn4" class="main">Porch off</button>
-<br>
-<button id="btn5" class="main">Vrum spot on</button>
-<button id="btn6" class="main">Vrum Spot off</button>
-<br>
-<button id="btn7" class="main">Månlampa on</button>
-<button id="btn8" class="main">Månlampa off</button>
-<br>
-<button id="btn9" class="main">Garden on</button>
-<button id="btn10" class="main">Garden off</button>
-<br>
-<button id="btn11" class="main">Vrum fönster on</button>
-<button id="btn12" class="main">Vrum fönster off</button>
-<br>
-<button id="btn13" class="main">Hallen on</button>
-<button id="btn14" class="main">Hallen off</button>
-<br>
-<button id="btn15" class="main">Kitchen on</button>
-<button id="btn16" class="main">Kitchen off</button>
-<br>
+<div id="wrap">
+	<div id="left">
+		<button id="btn1" class="main">Vrum on</button>
+		<button id="btn3" class="main">Porch on</button>
+		<button id="btn5" class="main">Vrum spot on</button>
+		<button id="btn7" class="main">Månlampa on</button>
+		<button id="btn9" class="main">Garden on</button>
+		<button id="btn11" class="main">Vrum fönster on</button>
+		<button id="btn13" class="main">Hallen on</button>
+		<button id="btn15" class="main">Kitchen on</button>
+	</div>
+	<div id="right">
+		<button id="btn2" class="main">Vrum off</button>
+		<button id="btn4" class="main">Porch off</button>
+		<button id="btn6" class="main">Vrum Spot off</button>
+		<button id="btn8" class="main">Månlampa off</button>
+		<button id="btn10" class="main">Garden off</button>
+		<button id="btn12" class="main">Vrum fönster off</button>
+		<button id="btn14" class="main">Hallen off</button>
+		<button id="btn16" class="main">Kitchen off</button>	
+	</div>
+</div>
 </body>
 </html>
